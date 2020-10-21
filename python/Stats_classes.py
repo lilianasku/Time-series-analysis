8171622bee608109b1f11f934855ad48690e069b
Liliana Skuljan <51868652+lilianasku@users.noreply.github.com>
2020-10-21 23:00:51 +1300

    Add files via upload

--- python/Stats_classes.py
+++ python/Stats_classes.py
@@ -13,9 +13,19 @@ def create_bootstraps(data_list, n_samples=1):
         bs_list.append(bs)
     yield bs_list
 
+#functon for generating one bootstrap replica
+def create_one_bootstrap(data_list, function):
+    assert len(data_list)!=0, 'Empty list'
+    bs=np.random.choice(data_list, size=len(data_list))
+    return function(bs)
+
+
 def main():
    l=[2,4,7,8]
    results=list(create_bootstraps(l,3))
+   result= create_one_bootstrap(l,np.mean)
+   print(result)
+
 
 if __name__=="__main__":
     main()
